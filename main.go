b2d040d96bcb4baf01a968d9a268fbcff0bde424
rogerio <rogeriopiatek@gmail.com>
2024-06-28 23:01:44 -0300

    feat: add block data model

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+// Data model to our block, containing the necessary fields like the PrevHash
+type Block struct {
+	Index     int
+	Timestamp string
+	BPM       int
+	Hash      string
+	PrevHash  string
+}
+
+var Blockchain []Block
+
 func main() {
 
 }

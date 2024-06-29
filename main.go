6c18b8b8089422cb7ee63f10c1b9112334bad14d
rogerio <rogeriopiatek@gmail.com>
2024-06-28 23:24:39 -0300

    feat: implement generateBlock function for creating new blocks

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"time"
 )
 
 // Data model to our block, containing the necessary fields like the PrevHash
@@ -24,6 +25,23 @@ func calculateHash(block Block) string {
 	h.Write([]byte(record))
 	hashed := h.Sum(nil)
 	return hex.EncodeToString(hashed)
+}
+
+// Needs to receive oldBlock so we get the PrevHash
+func generateBlock(oldBlock Block, BPM int) (Block, error) {
+	var newBlock Block
+
+	t := time.Now()
+
+	//auto increment the index based on previous block
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = t.String()
+	//BPM is receive as a parameter
+	newBlock.BPM = BPM
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+
+	return newBlock, nil
 }
 
 func main() {
